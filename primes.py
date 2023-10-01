82717991ad7bce6d9b5b96cbed27016a43c50439
Kewei Zhang <kewei.zhang@kcl.ac.uk>
2023-10-01 11:34:39 +0100

    Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    list.append(2)
+    candidate = list[-1] + 1
+    while len(list) < number_of_primes:
+        is_prime = True
+        for number in list:
+            if candidate % number == 0:
+                is_prime = False
+        if is_prime:
+            list.append(candidate)
+        candidate += 1
     return list
